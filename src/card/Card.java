0a1594530fa88e8fd832a808cba75a5125a45ec1
Dell <Dell@DESKTOP-EDHSQS7>
2024-01-30 23:12:53 -0500

    commit

--- src/card/Card.java
+++ src/card/Card.java
@@ -1,52 +1,73 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package card;
 
+import java.util.Scanner;
+
 /**
- * A class that models playing card Objects. Cards have 
- * a value (note that Ace = 1, Jack -11, Queen =12, King = 13)
- * A suit (clubs, hearts, spades, diamonds).
- * There are 52 cards in a deck, no jokers.
- * This code is to be used in ICE1. When you create your own branch,
- * add your name as a modifier.
- * @author srinivsi
+ * A class that fills a magic hand of 7 cards with random Card Objects
+ * and then asks the user to pick a card and searches the array of cards
+ * for the match to the user's card. To be used as starting code in ICE 1
+ * @author Nitika Nitika
+ * Student Number: 991715699
  */
-public class Card {
-
-   private String suit; //clubs, spades, diamonds, hearts
-   private int value;//1-13
-
-   public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
-    /**
-     * @return the suit
-     */
-    public String getSuit() {
-        return suit;
-    }
-
-    /**
-     * @param suit the suit to set
-     */
-    public void setSuit(String suit) {
-        this.suit = suit;
-    }
-
-    /**
-     * @return the value
-     */
-    public int getValue() {
-        return value;
-    }
-
-    /**
-     * @param value the value to set
-     */
-    public void setValue(int value) {
-        this.value = value;
-    }
-   
-   
+public class CardTrick {
     
+    public static void main(String[] args)
+    {
+        Card[] magicHand = new Card[7];
+        
+        // Fill the magic hand with random cards
+        for (int i = 0; i < magicHand.length; i++)
+        {
+            Card c = new Card();
+            c.setValue(generateRandomValue());
+            c.setSuit(Card.SUITS[generateRandomSuitIndex()]);
+            magicHand[i] = c;
+        }
+        
+        // Hardcode a luck card
+        Card luckCard = new Card();
+        luckCard.setValue(2);
+        luckCard.setSuit("Clubs");
+        magicHand[6] = luckCard; // Hardcoded at the last index
+        
+        // Prompt the user to input a card
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your card value (1-13): ");
+        int userValue = scanner.nextInt();
+        System.out.print("Enter your card suit (Hearts, Diamonds, Spades, Clubs): ");
+        String userSuit = scanner.next();
+        
+        // Create the user's card
+        Card userCard = new Card();
+        userCard.setValue(userValue);
+        userCard.setSuit(userSuit);
+        
+        // Search for the user's card in the magic hand
+        boolean found = false;
+        for (Card card : magicHand) {
+            if (card.getValue() == userCard.getValue() && card.getSuit().equalsIgnoreCase(userCard.getSuit())) {
+                found = true;
+                break;
+            }
+        }
+        
+        // Report the result
+        if (found) {
+            System.out.println("Congratulations! Your card is in the magic hand.");
+        } else {
+            System.out.println("Sorry, your card is not in the magic hand.");
+        }
+        
+        scanner.close();
+    }
+
+    // Helper method to generate a random card value (1-13)
+    private static int generateRandomValue() {
+        return (int) (Math.random() * 13) + 1;
+    }
+
+    // Helper method to generate a random suit index (0-3)
+    private static int generateRandomSuitIndex() {
+        return (int) (Math.random() * 4);
+    }
 }
